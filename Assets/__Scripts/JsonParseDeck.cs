5705f0c64b96d50f22d9e6f553e64debe3ac0313
Bill Crosbie <billcrosbie@users.noreply.github.com>
2025-03-09 16:03:28 -0400

    Add Static methods to JsonParseDeck
    easier to return parts of the data at a request

--- Assets/__Scripts/JsonParseDeck.cs
+++ Assets/__Scripts/JsonParseDeck.cs
@@ -18,28 +18,64 @@ public class JsonCard
 {                                                       // c
     public int rank;          // The rank (1-13) of this card
     public string face;          // Sprite to use for each face card
-    public List<JsonPip>pips = new List<JsonPip>(); // The pips on this card
+    public List<JsonPip> pips = new List<JsonPip>(); // The pips on this card
 }
 
 // This class contains information about the entire deck
 [System.Serializable]
 public class JsonDeck
 {                                                       // d
-    public List<JsonPip>decorators = new List<JsonPip>();
-    public List<JsonCard>cards = new List<JsonCard>();
+    public List<JsonPip> decorators = new List<JsonPip>();
+    public List<JsonCard> cards = new List<JsonCard>();
 }
 
 public class JsonParseDeck : MonoBehaviour
 {
+    private static JsonParseDeck S { get; set; } // Another automatic property
+
     [Header("Inscribed")]
     public TextAsset jsonDeckFile;  // Reference to the JSON_Deck text file 
 
     [Header("Dynamic")]
     public JsonDeck deck;
 
+
+
     void Awake()
     {
-        deck = JsonUtility.FromJson<JsonDeck>(jsonDeckFile.text);        
+        if (S != null)
+        {
+            Debug.LogError("JsonParseDeck.S can’t be set a 2nd time!");
+            return;
+        }
+        S = this;
+
+        deck = JsonUtility.FromJson<JsonDeck>(jsonDeckFile.text);
     }
 
+    /// <summary>
+    /// Returns the decorator layout information for all cards.
+    /// </summary>
+    static public List<JsonPip> DECORATORS
+    {
+        get { return S.deck.decorators; }
+    }
+
+    /// <summary>
+    /// Returns the JsonCard matching the rank passed in.
+    /// Note: The rank should be 1 (Ace) - 13 (King).
+    /// </summary>
+    /// <param name="rank">Must be an int in range 1-13</param>
+    /// <returns>JsonCard information</returns>
+    static public JsonCard GET_CARD_DEF(int rank)
+    {
+        if ((rank < 1) || (rank > S.deck.cards.Count))
+        {
+            Debug.LogWarning("Illegal rank argument: " + rank);
+            return null;
+        }
+        return S.deck.cards[rank - 1];
+    }
 }
+
+

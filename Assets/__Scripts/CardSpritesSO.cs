314fb3c37364cc5ce0773e85871f3a253dca0af9
Bill Crosbie <billcrosbie@users.noreply.github.com>
2025-03-09 15:44:31 -0400

    Add script for CardSprites Scriptable object

---
+++ Assets/__Scripts/CardSpritesSO.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CardSprites",
+menuName = "ScriptableObjects/CardSpritesSO")]         // a
+public class CardSpritesSO : ScriptableObject
+{                           // b
+    [Header("Card Stock")]
+    public Sprite cardBack;
+    public Sprite cardBackGold;
+    public Sprite cardFront;
+    public Sprite cardFrontGold;
+
+    [Header("Suits")]
+    public Sprite suitClub;
+    public Sprite suitDiamond;
+    public Sprite suitHeart;
+    public Sprite suitSpade;
+
+    [Header("Pip Sprites")]
+    public Sprite[] faceSprites;
+    public Sprite[] rankSprites;
+}

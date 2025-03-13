7f5f5303aad80261041b501e26bd5bb848b63c2b
Bill Crosbie <billcrosbie@users.noreply.github.com>
2025-03-13 16:19:25 -0400

    First Phase of layout mine
    Still need to set up sorting layers and adjust sorting order via script

--- Assets/__Scripts/Prospector.cs
+++ Assets/__Scripts/Prospector.cs
@@ -12,6 +12,12 @@ public class Prospector : MonoBehaviour
     [Header("Dynamic")]
     public List<CardProspector> drawPile;
 
+    public List<CardProspector> discardPile;
+    public List<CardProspector> mine;
+    public CardProspector target;
+
+    private Transform layoutAnchor;
+
     private Deck deck;
     private JsonLayout jsonLayout;
 
@@ -29,6 +35,8 @@ public class Prospector : MonoBehaviour
         Deck.Shuffle(ref deck.cards);
 
         drawPile = ConvertCardsToCardProspectors(deck.cards);
+
+        LayoutMine();
     }
 
     /// <summary>
@@ -49,5 +57,57 @@ public class Prospector : MonoBehaviour
         return (listCP);
     }
 
+    /// <summary>
+    /// Pulls a single card from the beginning of the drawPile and returns it
+    /// Note: There is no protection against trying to draw from an empty pile!
+    /// </summary>
+    /// <returns>The top card of drawPile</returns>
+    CardProspector Draw()
+    {
+        CardProspector cp = drawPile[0]; // Pull the 0th CardProspector
+        drawPile.RemoveAt(0);            // Then remove it from drawPile
+        return (cp);                      // And return it
+    }
+
+    /// <summary>
+    /// Positions the initial tableau of cards, a.k.a. the "mine"
+    /// </summary>
+    void LayoutMine()
+    {
+        // Create an empty GameObject to serve as an anchor for the tableau   // a
+        if (layoutAnchor == null)
+        {
+            // Create an empty GameObject named _LayoutAnchor in the Hierarchy
+            GameObject tGO = new GameObject("_LayoutAnchor");
+            layoutAnchor = tGO.transform;             // Grab its Transform
+        }
+
+        CardProspector cp;                                                    // b
+
+        // Iterate through the JsonLayoutSlots pulled from the JSON_Layout
+        foreach (JsonLayoutSlot slot in jsonLayout.slots)
+        {
+            cp = Draw(); // Pull a card from the top (beginning) of the draw Pile
+            cp.faceUp = slot.faceUp;    // Set its faceUp to the value in SlotDef
+                                        // Make the CardProspector a child of layoutAnchor
+            cp.transform.SetParent(layoutAnchor);
+
+            // Convert the last char of the layer string to an int (e.g. "Row 0")
+            int z = int.Parse(slot.layer[slot.layer.Length - 1].ToString());  // c
+
+            // Set the localPosition of the card based on the slot information
+            cp.SetLocalPos(new Vector3(
+            jsonLayout.multiplier.x * slot.x,
+            jsonLayout.multiplier.y * slot.y,
+            -z));                                                       // d
+
+            cp.layoutID = slot.id;
+            cp.layoutSlot = slot;
+            // CardProspectors in the mine have the state CardState.mine
+            cp.state = eCardState.mine;
+
+            mine.Add(cp); // Add this CardProspector to the List<mine
+        }
+    }
 
 }

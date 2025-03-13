ce22c1a81e37fca9c27abe1cd265474adc1be70d
Bill Crosbie <billcrosbie@users.noreply.github.com>
2025-03-13 15:58:36 -0400

    Remove Start method from Deck
    Was only used for testing purposes

--- Assets/__Scripts/Deck.cs
+++ Assets/__Scripts/Deck.cs
@@ -19,11 +19,13 @@ public class Deck : MonoBehaviour
 
     static public GameObject SPRITE_PREFAB { get; private set; }
 
+    /* TESTING MODE REMOVED
     void Start()
     {
         InitDeck();
         Shuffle(ref cards);
     }
+    */
 
     /// <summary>
     /// The Prospector class will call InitDeck to set up the deck and build
